b24449864a0f782922552f82f854f2708fe2d494
Nathan Balli <AeroStarCreations1@gmail.com>
2019-02-02 19:02:32 -0500

    displays initial state

--- lifeLogic.lua
+++ lifeLogic.lua
@@ -1,3 +1,5 @@
+local json = require("json")
+
 local world = {}
 local previousWorld = {}
 
--- lifeScene.lua
+++ lifeScene.lua
@@ -1,5 +1,6 @@
 local composer = require( "composer" )
 local lifeLogic = require( "lifeLogic" )
+local colors = require( "colors" )
  
 local scene = composer.newScene()
  
@@ -7,7 +8,48 @@ local scene = composer.newScene()
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
+local rows, cols, alive, coords
+local members = {}
  
+local w = display.actualContentWidth
+local h = display.actualContentHeight
+print("Width = "..w)
+print("Height = "..h)
+
+local function createMember(x, y, count, sceneGroup)
+    local memWidth = w / cols
+    local memHeight = h / rows
+    local memX = (x - 1) * memWidth
+    local memY = (y - 1) * memHeight
+
+    members[count] = display.newRoundedRect( 
+        sceneGroup, 
+        memX, 
+        memY, 
+        memWidth, 
+        memHeight, 
+        10 
+    )
+
+    members[count].anchorX = 0
+    members[count].anchorY = 0
+    members[count]:setFillColor(unpack(colors.green))
+end
+
+local function generateAliveMembers( sceneGroup )
+    members = {}
+    local world = lifeLogic.getCurrentState()
+    local count = 0
+    for i = 1, rows, 1 do
+        for j = 1, cols, 1 do
+            if world[i][j] == 1 then
+                print("("..i..", "..j..")")
+                count = count + 1
+                createMember(j, i, count, sceneGroup)
+            end
+        end
+    end
+end
  
  
  
@@ -32,19 +74,37 @@ function scene:show( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
-        if (event.params.numAlive) then
+        rows = event.params.numRows
+        cols = event.params.numCols
+        alive = event.params.numAlive
+        coords = event.params.coordinates
+
+        if (alive) then
             lifeLogic.generateInitialStateCustom(
-                event.params.numRows,
-                event.params.numCols,
-                event.params.numAlive
+                rows,
+                cols,
+                alive
             )
         else
             lifeLogic.generateInitialStateDefault(
-                event.params.numRows,
-                event.params.numCols,
-                event.params.coordinates
+                rows,
+                cols,
+                coords
             )
         end
+
+        generateAliveMembers( sceneGroup )
+
+        -- local bob = display.newRoundedRect( 
+        --     sceneGroup, 
+        --     0, 
+        --     0, 
+        --     w, 
+        --     h, 
+        --     10 
+        -- )
+        -- bob.anchorX = 0
+        -- bob.anchorY = 0
  
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
